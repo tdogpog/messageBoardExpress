1a87034663e240d64463ebb262d6e4aa6b558c00
tdog <161662619+tdogpog@users.noreply.github.com>
2024-11-08 18:43:34 -0600

    small changes for deployment

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use("/", indexRouter);
 app.use("/new", newRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`My first Express app - listening on port ${PORT}!`);
