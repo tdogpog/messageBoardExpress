8f3836c453e817634911175f6d9892bcf526eec0
tdog <161662619+tdogpog@users.noreply.github.com>
2024-11-18 17:24:19 -0600

    Updated scaffolding to handle serverside session storage

--- app.js
+++ app.js
@@ -3,13 +3,36 @@ const app = express();
 const path = require("node:path");
 const messagesRouter = require("./routes/messagesRouter");
 const authRouter = require("./routes/authRouter");
+
 //////////////////// AuthImports
 const session = require("express-session");
+const pool = require("./db/pool");
+require("dotenv").config({ path: "../.env" });
 
-//sess middleware
-app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+//sess middleware first to handle sess data for all routes
+app.use(
+  session({
+    store: new pgSession({
+      pool: pool, // The PostgreSQL connection pool
+      tableName: "session", // The table name where sessions will be stored
+    }),
+    secret: process.env.SESSION_SECRET, // A string used to sign the session ID cookie
+    resave: false, // Don't force save on every request (good for performance)
+    saveUninitialized: false, // Don't save uninitialized sessions (good for privacy)
+    cookie: {
+      secure: false, // Set to true in production if using HTTPS
+      maxAge: 15 * 60 * 1000, // Session expiration time, in milliseconds (e.g., 1 hour)
+    },
+  })
+);
+
+//passport middleware  comes after sess because it utilizes sess
 app.use(passport.session());
+
+//body parsing to handle forms
 app.use(express.urlencoded({ extended: true }));
+
+//view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
