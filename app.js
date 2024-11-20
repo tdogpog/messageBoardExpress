1dd89d0a2866b671b7359eb23eb2ec25e90e27d7
tdog <161662619+tdogpog@users.noreply.github.com>
2024-11-20 17:01:37 -0600

    Fixed incorrect routing:All ejs hrefs to messagesRouter should start with /messages/ROUTE, they were incorrectly referencing /ROUTE alone, causing a GET error.
    Changed req.session.user to req.user to grab
    information from passport rather than session.
    
    HTML changes on the view.

--- app.js
+++ app.js
@@ -50,5 +50,5 @@ app.use("/messages", messagesRouter);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`My first Express app - listening on port ${PORT}!`);
+  console.log(`Msgboard - listening on port ${PORT}!`);
 });
--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -3,21 +3,23 @@ const { insertMessage } = require("../db/queries");
 function renderNewMsgForm(req, res) {
   res.render("form", {
     title: "New Message",
+    unauthorized: "",
   });
 }
 
 async function postMessage(req, res) {
-  if (!req.session.user) {
+  console.log("req info given to the post", req.user);
+  if (!req.user) {
     return res.render("form", {
       title: "New Message",
       unauthorized: "Please log in to post a message",
     });
   }
   try {
-    await insertMessage(req.body.message, req.body.title, req.session.user.id);
+    await insertMessage(req.body.message, req.body.title, req.user.id);
     res.redirect("/");
   } catch (error) {
-    console.error("Error posting message:", error);
+    console.log("Error posting,", error.message);
     res.status(500).send("Error posting message");
   }
 }
